d57df57d2ca7b90e746329283726751c338f582e
David Davydov <kctrlv@gmail.com>
2016-10-03 22:23:45 -0600

    transaction show closes #8

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -9,14 +9,14 @@ describe 'Transactions API' do
     expect(res.count).to eq(3)
     expect(res.first['id']).to eq(transactions.first.id)
   end
-  #
-  # it 'returns a single Transaction' do
-  #   transaction = create(:transaction, name: 'Billy')
-  #   get "/api/v1/transactions/#{transaction.id}.json"
-  #   res = JSON.parse(response.body)
-  #   expect(response).to be_success
-  #   expect(res['name']).to eq('Billy')
-  # end
+
+  it 'returns a single Transaction' do
+    transaction = create(:transaction, credit_card_number: '1001200230034004')
+    get "/api/v1/transactions/#{transaction.id}.json"
+    res = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(res['credit_card_number']).to eq('1001200230034004')
+  end
   #
   # it 'returns a random Transaction' do
   #   merc1 = create(:transaction, name: 'Adam')
