e72ecb3733912f1160c3758d1841aad372eac300
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-10-03 20:44:48 -0600

    Add test for items show record

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -10,4 +10,14 @@ describe "Items CRUD API" do
     expect(response).to be_success
     expect(items.count).to eq(3)
   end
+
+  it "returns a single item" do
+    item = create(:item, name: "Sparkly Super Bounce Bouncy Ball")
+    get "/api/v1/items/#{item.id}.json"
+
+    raw_item = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(raw_item["name"]).to eq("Sparkly Super Bounce Bouncy Ball")
+  end
 end
