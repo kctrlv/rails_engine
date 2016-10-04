b33e2f51309feffdf4ec6179e59de67352662693
David Davydov <kctrlv@gmail.com>
2016-10-03 21:16:22 -0600

    merchants random closes #25

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,4 +6,8 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: Merchant.find(params['id'])
   end
+
+  def random
+    render json: Merchant.all.sample
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/api/v1/merchants/random', to: 'api/v1/merchants#random'
+
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
