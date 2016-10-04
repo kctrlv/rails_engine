7be762017a339efc89cc145dc50fd71ea283c4fd
David Davydov <kctrlv@gmail.com>
2016-10-03 20:36:49 -0600

    route added for merchant index

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,22 +8,4 @@ describe 'Merchants API' do
     expect(response).to be_success
     expect(res.count).to eq(3)
   end
-  #
-  # it 'returns one item' do
-  #   item = create(:item, name: "Cool Item")
-  #   get "/api/v1/items/#{item.id}"
-  #   res = JSON.parse(response.body)
-  #   expect(response).to be_success
-  #   expect(res['name']).to eq(item.name)
-  # end
-  #
-  # it 'creates a new item' do
-  #   parameters = {name: "Cookie", description: "Vanilla"}
-  #   expect {
-  #     post '/api/v1/items', parameters
-  #   }.to change { Item.count }.by(1)
-  #   res = JSON.parse(response.body)
-  #   expect(res['name']).to eq(parameters[:name])
-  #   expect(res['description']).to eq(parameters[:description])
-  # end
 end
