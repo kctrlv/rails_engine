5dac8e7a3bf07ec99f757c24a5ec73e48d2e795b
David Davydov <kctrlv@gmail.com>
2016-10-03 20:49:33 -0600

    merchants index working

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 describe 'Merchants API' do
   it 'returns a list of Merchants' do
-    create_list(:merchant, 3)
+    merchants = create_list(:merchant, 3)
     get '/api/v1/merchants.json'
     res = JSON.parse(response.body)
     expect(response).to be_success
     expect(res.count).to eq(3)
+    expect(res.first['id']).to eq(merchants.first.id)
   end
 end
