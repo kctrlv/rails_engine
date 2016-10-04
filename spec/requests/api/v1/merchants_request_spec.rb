b4ba27f4766094ee79582c7d632eba7126cd9ab4
David Davydov <kctrlv@gmail.com>
2016-10-03 20:56:29 -0600

    merchant show spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -9,4 +9,12 @@ describe 'Merchants API' do
     expect(res.count).to eq(3)
     expect(res.first['id']).to eq(merchants.first.id)
   end
+
+  it 'returns a single Merchant' do
+    merchant = create(:merchant, name: 'Billy')
+    get "/api/v1/merchants/#{merchant.id}.json"
+    res = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(res['name']).to eq('Billy')
+  end
 end
