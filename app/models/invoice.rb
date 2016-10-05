0f30143ec25bd07d00daf6d9d6a153072c2150a6
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-10-05 15:44:24 -0600

    Add paid in full? method for invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,21 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
+
+  has_many :transactions
+  has_many :invoice_items
+
+  def total
+    
+  end
+
+  def has_transactions?
+    !transactions.empty?
+  end
+
+  def paid_in_full?
+    has_transactions? && transactions.any? do |transaction|
+      transaction.result == "success"
+    end
+  end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,11 +5,43 @@ RSpec.describe Invoice, type: :model do
 
   it { should belong_to(:merchant) }
 
-  it "should calculate its own total revenue" do
+  it { should have_many(:transactions) }
+
+  it { should have_many(:invoice_items) }
+
+  xit "should calculate its own total revenue" do
     invoice = create(:invoice)
-    ii_1 = create(:invoice_item, invoice: invoice, quantity: 10, unit_price: 10.00)
-    ii_2 = create(:invoice_item, invoice: invoice, quantity: 2, unit_price: 50.00)
+              create(:invoice_item, invoice: invoice, quantity: 10, unit_price: 10.00)
+              create(:invoice_item, invoice: invoice, quantity: 2, unit_price: 50.00)
+              create(:transaction, invoice: invoice, result: "success")
+
 
     expect(invoice.total).to eq(200.00)
+  end
+
+  it "can check if it has no transactions" do
+    invoice = create(:invoice)
+
+    expect(invoice.has_transactions?).to be false
+  end
+
+  context "when paid in full" do
+    it "returns as paid in full" do
+      invoice = create(:invoice)
+                create(:transaction, invoice: invoice, result: "failed")
+                create(:transaction, invoice: invoice, result: "success")
+
+      expect(invoice.paid_in_full?).to be true
+    end
+  end
+
+  context "when not paid in full" do
+    it "returns as not paid in full" do
+      invoice = create(:invoice)
+                create(:transaction, invoice: invoice, result: "failed")
+                create(:transaction, invoice: invoice, result: "failed")
+
+      expect(invoice.paid_in_full?).to be false
+    end
   end
 end
