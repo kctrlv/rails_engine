1d7ec7d7903238cc146a0bcc34d971aa805ebbde
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-10-06 12:53:56 -0600

    Add activerecord for Mercahnt method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,18 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def revenue
-    invoices.map { |invoice| invoice.total }.sum
+    # invoices.map { |invoice| invoice.total }.sum
+    byebug
+
+  end
+
+  def merchant_paid_invoice_items
+    paid_invoices.map do |invoice|
+      InvoiceItem.where(invoice_id: invoice)
+    end
+  end
+
+  def paid_invoices
+    invoices.joins(:transactions).where(transactions: { result: 'success' })
   end
 end
